8c1b616143f9a869c978b28e8883bb1f6e32e198
quintentwisk <quinten.twisk@hotmail.com>
2021-06-07 19:36:19 +0200

    Basic datalogger, can open file on SD card an write to it

---
+++ src/Datalogger/Datalogger.ino
@@ -0,0 +1,42 @@
+#include <SPI.h>
+#include <SD.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.print("Initializing SD card...");
+  
+  if (!SD.begin()) {
+    Serial.println("Card failed, or not present");
+    while (1);
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  delay(1000);
+  String dataString = "";
+
+  for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 2) {
+      dataString += ",";
+    }
+  }
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    Serial.println(dataString);
+  }
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
